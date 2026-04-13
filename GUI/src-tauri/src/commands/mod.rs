9954819169a2e8f5645b31f189ca01e78d277963
mikewolfli <mikewolfli@163.com>
2026-04-13 23:59:33 +0800

    t

--- GUI/src-tauri/src/commands/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod config;
-pub mod health;
-pub mod integrations;
-pub mod logs;
-pub mod metrics;
-pub mod process;
-pub mod runtime_ops;
--- GUI/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export function buildClient(baseURL = "http://127.0.0.1:8090") {
-    return axios.create({
-        baseURL,
-        timeout: 4000,
-    });
-}
